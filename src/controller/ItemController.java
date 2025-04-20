f13f855cd55768b86d7973be147dc37a56323303
mingjia <lmj11083@gmail.com>
2025-04-20 12:29:08 +0800

    Done with item backend CRUD logic

--- src/controller/ItemController.java
+++ src/controller/ItemController.java
@@ -1,12 +1,8 @@
 package controller;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Date;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 public class ItemController {
     private String itemId;
@@ -16,83 +12,101 @@ public class ItemController {
     private String unit;
     private String createdAt;
     private String updatedAt;
-    private String createBy;
+    private String createdBy;
     private String updatedBy;
     private String itemDetailsFile = "data/item_details.txt";
+    private FileController fileController = new FileController(itemDetailsFile);
 
-    public ItemController(String itemId, String itemName, int quantity, String unit, String supplierId){
-        // Constructor
-        this.itemId = itemId;
-        this.itemName = itemName;
-        this.quantity = quantity;
-        this.unit = unit;
-        this.supplierId = supplierId;
-        this.createdAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        this.updatedAt = createdAt;
+    public ItemController() {
+        // Default constructor
     }
 
-    public String getAllItems() {
+    public List<String> getAllItems() {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(itemDetailsFile));
-            ArrayList<String> items = new ArrayList<>();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                items.add(line);
+            List<String> lines = fileController.getFile();
+            if (lines == null || lines.isEmpty()) {
+                System.out.println("No items found");
+                return null;
             }
-            reader.close();
-            return items.toString();
+            return lines;
         } catch (Exception e) {
-            return "Error occurs: " + e.getMessage();
+            e.printStackTrace();
+            return null;
         }
     }
 
-    public void addItem() {
+    public String getOneItemWithId(String itemId) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(itemDetailsFile, true));
-            writer.write(itemId + "," + itemName + "," + quantity + "," + supplierId + "," + createdAt + "," + updatedAt);
-            System.out.println("Item added successfully: " + itemId + "|" + itemName + "|" + quantity + "|" + supplierId + "|" + createdAt + "|" + updatedAt);
-            writer.close();
+            String[] itemDetails = fileController.getLine(0, itemId);
+            if (itemDetails != null) {
+                System.out.println("Item found: " + String.join(",", itemDetails));
+                return String.join(",", itemDetails);
+            } else {
+                System.out.println("Item not found");
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void addItem(String itemName, int quantity, String unit, String supplierId) {
+        try {
+            int tempItemId = fileController.getFile().size()+1;
+            this.itemId = "IM" + String.format("%03d", tempItemId);
+            this.itemName = itemName;
+            this.quantity = quantity;
+            this.unit = unit;
+            this.supplierId = supplierId;
+            this.createdAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            this.updatedAt = createdAt;
+            this.createdBy = SessionController.getInstance().getUserId();
+            this.updatedBy = SessionController.getInstance().getUserId();
+            String data = String.join(",", itemId, itemName, String.valueOf(quantity), unit, supplierId, createdAt, updatedAt, createdBy, updatedBy);
+            fileController.appendFile(data);
+            System.out.println("Item added successfully");
         }
         catch (Exception e) {
-            System.out.println("Error adding item to text file: " + e.getMessage());
+            e.printStackTrace();
+            System.out.println("Failed to add item: " + e.getMessage());
         }
     }
 
-    public void updateItem() {
+    public void updateItem(String itemId, String itemName, int quantity, String unit, String supplierId) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(itemDetailsFile));
-            ArrayList<String> items = new ArrayList<>();
-            items.add(reader.readLine());
-            System.out.println("Item details read successfully:"+items);
-//            for (item: items) {
-//                if (item[0].equals(itemId)) {
-//                    item[1] = itemName;
-//                    item[2] = String.valueOf(quantity);
-//                    item[3] = supplierId;
-//                    item[4] = createdAt;
-//                    item[5] = updatedAt;
-//                }
-//                items.add(String.join(",", item));
-//            }
-            reader.close();
-//            BufferedWriter writer = new BufferedWriter(new FileWriter(itemDetailsFile));
-//            for (String item : items) {
-//                writer.write(item);
-//                writer.newLine();
-//            }
-//            writer.close();
-//            System.out.println("Item updated successfully: " + itemId + "|" + itemName + "|" + quantity + "|" + supplierId);
+            String[] existingItemDetails = fileController.getLine(0, itemId);
+            if (existingItemDetails == null) {
+                System.out.println("Item not found");
+                return;
+            }
+            this.itemId = itemId;
+            this.itemName = (itemName != null) ? itemName : existingItemDetails[1];
+            this.quantity = (quantity != 0) ? quantity : Integer.parseInt(existingItemDetails[2]);
+            this.unit = (unit != null) ? unit : existingItemDetails[3];
+            this.supplierId = (supplierId != null) ? supplierId : existingItemDetails[4];
+            this.createdAt = existingItemDetails[5];
+            this.updatedAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            this.createdBy = existingItemDetails[7];
+            this.updatedBy = SessionController.getInstance().getUserId();
+
+            String data = String.join(",", this.itemId, this.itemName, String.valueOf(this.quantity), this.unit, this.supplierId, createdAt, updatedAt, createdBy, updatedBy);
+            fileController.updateFile(data);
+            System.out.println("Item updated successfully");
         }
         catch (Exception e) {
-            System.out.println("Error updating item in text file: " + e.getMessage());
+            e.printStackTrace();
+            System.out.println("Failed to update item: " + e.getMessage());
         }
     }
 
-    public void deleteItem() {
-        // Logic to delete an item
-    }
-
-    public void viewItems() {
-        // Logic to view all items
+    public void deleteItem(String itemId) {
+        try {
+            fileController.deleteLine(itemId, 0);
+            System.out.println("Item deleted successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to delete item: " + e.getMessage());
+        }
     }
 }
