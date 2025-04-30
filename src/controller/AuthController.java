b5fd96885de4d4ba290b8192e4bd0f6dad765cd4
Lee Chen Wei <leechenwei7@gmail.com>
2025-04-30 21:48:04 +0800

    update argument email for model call)

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -20,25 +20,26 @@ public class AuthController {
                 String userId = details[0].trim();
                 String firstName = details[3].trim();
                 String lastName = details[4].trim();
-                String status = details[5].trim();
-                String createdAt = details[6].trim();
-                String updatedAt = details[7].trim();
+                String email = details[5].trim();
+                String status = details[6].trim();
+                String createdAt = details[7].trim();
+                String updatedAt = details[8].trim();
                 String roleCode = userId.substring(0, 2);
 
                 User user = null;
                 switch (roleCode) {
                     case "AM":
-                        user = new Admin(userId, username, password, firstName, lastName, status, createdAt, updatedAt);
+                        user = new Admin(userId, username, password, firstName, lastName, email, status, createdAt, updatedAt);
                         break;
                     case "SM":
-                        user = new SalesManager(userId, username, password, firstName, lastName, createdAt, updatedAt);
+                        user = new SalesManager(userId, username, password, firstName, lastName, email, status, createdAt, updatedAt);
                         break;
                     case "PM":
-                        user = new PurchaseManager(userId, username, password, firstName, lastName, createdAt, updatedAt);
+                        user = new PurchaseManager(userId, username, password, firstName, lastName, email, status, createdAt, updatedAt);
                     case "IM":
-                        user = new InventoryManager(userId, username, password, firstName, lastName, createdAt, updatedAt);
+                        user = new InventoryManager(userId, username, password, firstName, lastName, email, status, createdAt, updatedAt);
                     case "FM":
-                        user = new FinanceManager(userId, username, password, firstName, lastName, createdAt, updatedAt);
+                        user = new FinanceManager(userId, username, password, firstName, lastName, email, status, createdAt, updatedAt);
                     default:
                         break;
                 }
