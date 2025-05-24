2aa15d52096930c33537107baeeb1b7b4009144f
mingjia <lmj11083@gmail.com>
2025-05-25 05:58:24 +0800

    Modified item details and seeder

--- src/seeders/ItemSeeder.java
+++ src/seeders/ItemSeeder.java
@@ -17,11 +17,11 @@ public class ItemSeeder {
         String createdAt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(itemDetailsFile, false));
-            writer.write("IM001,Seeder Item A,100,kg,100,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
-            writer.write("IM002,Seeder Item B,200,l,20,SUP002,"+createdAt+","+createdAt+",sales1,sales1\n");
-            writer.write("IM003,Seeder Item C,100,kg,200,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
-            writer.write("IM004,Seeder Item D,100,kg,15,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
-            writer.write("IM005,Seeder Item E,100,kg,25.5,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
+            writer.write("IM001,A12321,Seeder Item A,100,kg,100,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
+            writer.write("IM002,B12923,Seeder Item B,200,l,20,SUP002,"+createdAt+","+createdAt+",sales1,sales1\n");
+            writer.write("IM003,C01028,Seeder Item C,100,kg,200,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
+            writer.write("IM004,D81891,Seeder Item D,100,kg,15,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
+            writer.write("IM005,E45642,Seeder Item E,100,kg,25.5,SUP001,"+createdAt+","+createdAt+",sales1,sales1\n");
             System.out.println("Successfully Seeded data to item_details.txt");
             writer.close();
         } catch (Exception e) {
