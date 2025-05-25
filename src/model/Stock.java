158412bbca3e97422c6b91e6d783953dec18ae94
Carol Hew <hew78pikrou@gmail.com>
2025-05-25 21:53:21 +0800

    add Stock model class

---
+++ src/model/Stock.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Stock {
+    private String id;
+    private String name;
+    private int currentStock;
+    private int minStock;
+    private int maxStock;
+    private String status;
+    private String lastUpdateDate;
+
+    public Stock(String id, String name, int currentStock, int minStock, int maxStock, String status,
+                 String lastUpdateDate) {
+        this.id = id;
+        this.name = name;
+        this.currentStock = currentStock;
+        this.minStock = minStock;
+        this.maxStock = maxStock;
+        this.status = status;
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public String getId() {return id;};
+    public void setId(String id) {this.id = id;};
+
+    public String getName() {return name;};
+    public void setName(String name) {this.name = name;};
+
+    public int getCurrentStock() {return currentStock;};
+    public void setCurrentStock(int currentStock) {this.currentStock = currentStock;}
+
+    public int getMinStock() {return minStock;};
+    public void setMinStock(int minStock) {this.minStock = minStock;}
+
+    public int getMaxStock() {return maxStock;};
+    public void setMaxStock(int maxStock) {this.maxStock = maxStock;}
+
+    public String getStatus() {return status;};
+    public void setStatus(String status) {this.status = status;}
+
+    public String getLastUpdateDate() {return lastUpdateDate;};
+    public void setLastUpdateDate(String lastUpdateDate) {this.lastUpdateDate = lastUpdateDate;}
+
+    @Override
+    public String toString() {
+        return String.join(",",
+                id,
+                name,
+                String.valueOf(currentStock),
+                String.valueOf(minStock),
+                String.valueOf(maxStock),
+                status,
+                lastUpdateDate
+                );
+    }
+}
