68cdc4c5500f8d21d2b778e4f236c7f409e48663
IVANS-LAPTOP\Ivan Soo <ivansoo10@gmail.com>
2025-06-03 01:39:38 +0800

    po management

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -101,8 +101,8 @@ public class AuthController extends CRUDController<User>{
                 new InventoryManagerView((InventoryManager) user, menuItems, quickOptions);
                 break;
             case "FM":
-                String[] financeMenu = {"View Requisitions", "View Purchase Orders", "Financial Management"};
-                String[] financeOptions = {"View Requisitions", "View Purchase Orders", "Financial Management"};
+                String[] financeMenu = {"View Requisitions", "View Purchase Orders", "Purchase Order Approval", "Purchase Order Management"};
+                String[] financeOptions = {"View Requisitions", "View Purchase Orders", "Purchase Order Approval", "Purchase Order Management"};
                 new FMView((FinanceManager) user, financeMenu, financeOptions);
                 break;
             default:
